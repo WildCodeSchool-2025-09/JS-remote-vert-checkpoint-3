deee07f334b19a4bb53284f0bd746ebb5751d44e
abijoserah <serahabijo94@gmail.com>
2026-01-19 13:13:55 +0100

    step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -11,7 +11,18 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+    const tiles = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (tiles.length === 0) {
+      res.sendStatus(422);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
