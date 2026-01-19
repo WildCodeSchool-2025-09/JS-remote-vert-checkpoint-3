9696062ae501bb1f85c4042670bb51bfab343879
Lollye-dev <lorenvisset@gmail.com>
2026-01-19 14:17:09 +0100

    test step4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -13,6 +13,34 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    const coordX = req.body.coord_x;
+    const coordY = req.body.coord_y;
+
+    if (
+      coordX === undefined ||
+      coordY === undefined ||
+      Number.isNaN(coordX) ||
+      Number.isNaN(coordY)
+    ) {
+      res.sendStatus(422);
+      return;
+    }
+
+    const tilesAtCoordinates = await tileRepository.readByCoordinates(
+      coordX,
+      coordY,
+    );
+
+    if (tilesAtCoordinates.length === 0) {
+      res.sendStatus(422);
+      return;
+    }
+
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
