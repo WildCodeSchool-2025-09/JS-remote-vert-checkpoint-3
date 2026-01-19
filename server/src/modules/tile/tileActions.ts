ecf800059bab6ec3d303c4dde63ab3da1bb8c710
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2026-01-19 11:46:49 +0100

    step 4 COMPLETED

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,7 +12,17 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  const { coord_x, coord_y } = req.body;
+  const tile = await tileRepository.readByCoordinates(
+    Number(coord_x),
+    Number(coord_y),
+  );
+
+  if (tile.length === 0) {
+    res.sendStatus(422);
+  } else {
+    next();
+  }
 };
 
 export default {
