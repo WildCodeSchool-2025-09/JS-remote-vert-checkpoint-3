6731d9cca24d9d21f18427e38eed5678ee108a06
Florent Goujon <gouflo@gmail.com>
2026-01-19 11:58:56 +0100

    step5

--- server/src/router.ts
+++ server/src/router.ts
@@ -5,9 +5,9 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-import tileActions from "./modules/tile/tileActions";
 import boatActions from "./modules/boat/boatActions";
 import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
 
 router.get("/api/boats", boatActions.browse);
 router.put("/api/boats/:id", tileActions.validate, boatActions.edit);
