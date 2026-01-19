1f70febd20a8465a066972817d81a65bf198d58f
jeremy gontier <jeremy.gontier@gmail.com>
2026-01-19 12:11:57 +0100

    step5 ok

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -11,6 +11,34 @@ type Boat = {
 
 class BoatRepository {
   async readAll(where?: { name: string }) {
+    const [rows] = await databaseClient.query<Rows>(
+      `
+    SELECT
+      boat.id AS boat_id,
+      boat.name AS boat_name,
+      boat.coord_x AS boat_coord_x,
+      boat.coord_y AS boat_coord_y,
+      tile.type AS tile_type,
+      tile.has_treasure AS tile_has_treasure
+    FROM boat
+    LEFT JOIN tile
+      ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y
+    ORDER BY boat.coord_y, boat.coord_x
+    `,
+    );
+
+    return rows.map((row) => ({
+      id: row.boat_id,
+      name: row.boat_name,
+      coord_x: row.boat_coord_x,
+      coord_y: row.boat_coord_y,
+      type: row.tile_type,
+      has_treasure: !!row.tile_has_treasure,
+    }));
+  }
+
+  /*
+  async readAll(where?: { name: string }) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
       "select * from boat order by coord_y, coord_x",
@@ -18,7 +46,7 @@ class BoatRepository {
 
     // Return the array of tiles
     return rows as Boat[];
-  }
+  }*/
 
   async update(boatToUpdate: Partial<Boat>) {
     // your code here
