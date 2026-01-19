f7015e827c2b82106d4f805b93e427ad01658791
Emericodes <pro.emeric.m@gmail.com>
2026-01-19 11:14:11 +0100

    step 2.2: create GET/ api / tiles modification 2 files routes.ts and tileactions.ts

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,16 @@
 import type { RequestHandler } from "express";
 
+import tileRepository from "./tileRepository";
+
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  const tiles = await tileRepository.readAll();
+  try {
+    // Respond with the boats in JSON format
+    res.json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
