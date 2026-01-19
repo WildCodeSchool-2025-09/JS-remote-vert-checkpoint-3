f94db8bec742c46fa5771cf0e4a57ce95091dd93
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2026-01-19 10:00:02 +0100

    step 2 finished

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,14 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const invitations = await tileRepository.readAll();
+
+    res.json(invitations);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
