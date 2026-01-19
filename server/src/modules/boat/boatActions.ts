f6064471f33befc13384fab242c6a6897bb77477
Lisa Bureau <bureau.lisa03@gmail.com>
2026-01-19 10:20:32 +0100

    finished step 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,19 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  const boatToUpdate = {
+    id: Number(req.params.id),
+    coord_x: req.body.coord_x,
+    coord_y: req.body.coord_y,
+  };
+
+  const affectedRows = await boatRepository.update(boatToUpdate);
+
+  if (!affectedRows) {
+    res.sendStatus(404);
+  } else {
+    res.sendStatus(204);
+  }
 };
 
 export default {
