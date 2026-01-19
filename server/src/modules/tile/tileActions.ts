a60c3897061fca7c86e4d77d2da5d6f38f4d00db
Uldyssian <mickael.brouttier@gmail.com>
2026-01-19 11:54:30 +0100

    Step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -33,7 +33,30 @@ const edit: RequestHandler = async (req, res, next) => {
   }
 };
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const coord_x = Number(req.body.coord_x);
+    const coord_y = Number(req.body.coord_y);
+
+    if (Number.isNaN(coord_x) || Number.isNaN(coord_y)) {
+      res.sendStatus(422);
+      return;
+    }
+
+    if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422);
+      return;
+    }
+
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (!tile) {
+      res.sendStatus(404);
+      return;
+    }
+  } catch (err) {
+    next(err);
+  }
+  next();
 };
 
 export default {
