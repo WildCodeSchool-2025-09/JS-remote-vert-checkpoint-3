6b0082ac70eadd6777fc468f31c161310c158bec
Yuliana Krasulya <krasulyayuliana@gmail.com>
2026-01-19 09:53:20 +0100

    step2 add GET /api/tiles

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,19 +1,14 @@
 import express from "express";
 
+import boatActions from "./modules/boat/boatActions";
+import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
+
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-import boatActions from "./modules/boat/boatActions";
-
 router.get("/api/boats", boatActions.browse);
-
-import gameActions from "./modules/game/gameActions";
+router.get("/api/tiles", tileActions.browse);
 
 router.post("/api/games", gameActions.add);
-
-/* ************************************************************************* */
 
 export default router;
