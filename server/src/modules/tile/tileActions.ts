7fdbe7ee2f580dceaa48911edf4d10c787c71ff6
Rieu Thomas <rieu.thomas@hotmail.com>
2026-01-19 12:09:24 +0100

    finished step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,7 +12,20 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  const { coord_x, coord_y } = req.body;
+
+  if (
+    typeof coord_x !== "number" ||
+    typeof coord_y !== "number" ||
+    coord_x < 0 ||
+    coord_x > 11 ||
+    coord_y < 0 ||
+    coord_y > 5
+  ) {
+    res.sendStatus(422);
+  }
+
+  next();
 };
 
 export default {
