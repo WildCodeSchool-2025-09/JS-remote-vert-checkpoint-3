54cc639dba12ae12a0f6cc236ed1d38af8600b78
Colombine972 <decidela972@me.com>
2026-01-19 12:10:41 +0100

    finish test step4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,7 +12,25 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const coord_x = req.body.coord_x;
+    const coord_y = req.body.coord_y;
+
+    if (typeof coord_x !== "number" || typeof coord_y !== "number") {
+      res.sendStatus(422);
+      return;
+    }
+
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (tile.length === 0) {
+      res.sendStatus(422);
+      return;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
