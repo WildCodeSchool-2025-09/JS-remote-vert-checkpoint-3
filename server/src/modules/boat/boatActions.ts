0f6bbaa902b11baa32acc17a1c33fc5b38d16984
ClarissaCardarelli <ccardare@gmail.com>
2026-01-19 10:00:17 +0100

    step 3 done - boat edit route and action done

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -1,5 +1,7 @@
 import type { RequestHandler } from "express";
 
+import { StatusCodes } from "http-status-codes";
+
 import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
@@ -16,7 +18,24 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boat = {
+      id: Number(req.params.id),
+      name: req.body.name,
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await boatRepository.update(boat);
+
+    if (affectedRows === 0) {
+      res.sendStatus(StatusCodes.NOT_FOUND);
+    } else {
+      res.sendStatus(StatusCodes.NO_CONTENT);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
