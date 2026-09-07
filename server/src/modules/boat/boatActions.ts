6e84a7071c24ac4c2cdfdbc0c0307db4eaf685f1
czagoh <dorian.pennegat@hotmail.fr>
2026-09-07 10:46:17 +0200

    step 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,21 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const id = Number(req.params.id);
+
+    const boatUpdate = {
+      id: Number(req.params.id),
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    await boatRepository.update(boatUpdate);
+
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
