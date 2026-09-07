6466a917733caf9799c40cc52ab51ff8dd3daefb
alexandravelisar <anda_alexandra91@yahoo.com>
2026-09-07 11:13:27 +0200

    add PUT route to update boat coordinates

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -4,19 +4,32 @@ import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const boats = await boatRepository.readAll();
 
-    // Respond with the boats in JSON format
     res.json(boats);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boatToUpdate = {
+      id: Number(req.params.id),
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await boatRepository.update(boatToUpdate);
+
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
