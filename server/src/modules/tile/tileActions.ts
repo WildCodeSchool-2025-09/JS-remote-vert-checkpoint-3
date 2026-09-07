66f0ff28f12d6cb4dee98436cc768c283a891116
Guillaume Galinanes <galinanesgm@gmail.com>
2026-09-07 11:21:13 +0200

    step2 passed

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,14 +1,24 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+	try {
+		// Fetch all tiles from the database
+		const tiles = await tileRepository.readAll();
+
+		// Respond with the tiles in JSON format
+		res.json(tiles);
+	} catch (err) {
+		// Pass any errors to the error-handling middleware
+		next(err);
+	}
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+	// your code here
 };
 
 export default {
-  browse,
-  validate,
+	browse,
+	validate,
 };
