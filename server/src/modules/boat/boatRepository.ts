58ef5fa7f9670fc77c4874d70ee60527c33a8ad0
alexandravelisar <anda_alexandra91@yahoo.com>
2026-09-07 12:04:47 +0200

    add boat name filter

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -10,24 +10,32 @@ type Boat = {
 };
 
 class BoatRepository {
-  async readAll(where?: { name: string }) {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await databaseClient.query<Rows>(
-      `select
-    boat.id,
-    boat.coord_x,
-    boat.coord_y,
-    boat.name,
-    tile.type,
-    tile.has_treasure
-  from boat
-  join tile
-    on boat.coord_x = tile.coord_x
-    and boat.coord_y = tile.coord_y
-  order by boat.coord_y, boat.coord_x`,
-    );
+  async readAll(where?: { name?: string }) {
+    let sql = `
+    select
+      boat.id,
+      boat.coord_x,
+      boat.coord_y,
+      boat.name,
+      tile.type,
+      tile.has_treasure
+    from boat
+    join tile
+      on boat.coord_x = tile.coord_x
+      and boat.coord_y = tile.coord_y
+  `;
 
-    // Return the array of tiles
+    const values: string[] = [];
+
+    if (where?.name) {
+      sql += " where boat.name = ?";
+      values.push(where.name);
+    }
+
+    sql += " order by boat.coord_y, boat.coord_x";
+
+    const [rows] = await databaseClient.query<Rows>(sql, values);
+
     return rows as Boat[];
   }
 
