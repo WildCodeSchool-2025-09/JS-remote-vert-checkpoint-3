4b7534025d9f9fc6c50d478ae74b345f4c2c1068
Guillaume Galinanes <galinanesgm@gmail.com>
2026-09-07 13:19:56 +0200

    Step3 passed

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,24 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const id = Number(req.params.id);
+    const coordX = req.body.coord_x;
+    const coordY = req.body.coord_y;
+
+    const affectedRows = await boatRepository.update({
+      id: id,
+      coord_x: coordX,
+      coord_y: coordY,
+    });
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
